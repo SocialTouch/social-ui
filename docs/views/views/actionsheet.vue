7fb8aaff8bf8d56096cbdca15350ee8709a0b403
丁磊 <dl375074900@qq.com>
2016-09-14 00:02:12 +0800

    增加默认类型和分享类型操作按钮控制

--- docs/views/views/actionsheet.vue
+++ docs/views/views/actionsheet.vue
@@ -9,15 +9,18 @@
 			<div style="margin: 0px 15px;">
 				<div style="margin: 15px 0px;" v-for="item in list">
 					<button type="button" class="am-button am-button-ghost" @click="opend(item)">
-						<span>{{item}}</span>
+						<span>{{item.name}}</span>
 					</button>
+					
 				</div>
 			</div>
 		</div>
 	</div>
 
 	<soc-actionsheet 
-		:message="actionsheet.title"
+		:type="actionsheet.type"
+		:title="actionsheet.title"
+		:message="actionsheet.message"
 		:show="actionsheet.show">
 	</soc-actionsheet>
 
@@ -35,16 +38,32 @@
 		},
 		data () {
 			return {
-				list: ['默认状态操作列表','带分享功能的操作列表','带多行按钮的分享功能操作列表'],
-				actionsheet: {
-					message: '1111',
-					show: false
-				}
+				list: [
+					{name:'默认状态操作列表',type:'default',message: '我是描述我是描述',show: false,title:'我是标题',
+						options:[
+							{name:'操作0'},
+							{name:'操作1'}
+						]
+					},
+					{name:'带分享功能的操作列表',type:'share',message: '我是描述我是描述',show: false,title:'我是标题',
+						options:[
+							{name:'操作0'},
+							{name:'操作1'}
+						]
+					},
+					{name:'带多行按钮的分享功能操作列表',type:'share',message: '我是描述我是描述',show: false,title:'我是标题',
+						options:[
+							{name:'操作0'},
+							{name:'操作1'}
+						]
+					}
+				],
+				actionsheet: {}
 			}
 		},
 		methods: {
 			opend ( obj ) {
-				this.actionsheet.show = true
+				this.actionsheet = obj
 			}
 		}
 	}
--- src/components/views/actionsheet.vue
+++ src/components/views/actionsheet.vue
@@ -12,7 +12,7 @@
 						<h3 class="am-action-sheet-title" v-if="title!=''">{{title}}</h3>
 						<div class="am-action-sheet-message">{{message}}</div>
 
-						<ul class="am-action-sheet-button-list">
+						<ul class="am-action-sheet-button-list" v-if="type=='default'">
 							<li class="am-action-sheet-button-list-item">操作 0</li>
 							<li class="am-action-sheet-destructive-button">删除</li>
 							<li class="am-action-sheet-cancel-button">
@@ -21,11 +21,48 @@
 							</li>
 						</ul>
 
-						<div class="am-action-sheet-share">
-							
+						<div class="am-action-sheet-share" v-else>
+							<div class="am-action-sheet-share-list">
+								<div class="am-action-sheet-share-list-item">
+									<div class="am-action-sheet-share-list-item-icon">
+										<i type="mail" class="anticon anticon-mail"></i>
+									</div>
+									<div class="am-action-sheet-share-list-item-title">发邮件</div>
+								</div>
+								<div class="am-action-sheet-share-list-item">
+									<div class="am-action-sheet-share-list-item-icon">
+										<i type="message" class="anticon anticon-message"></i>
+									</div>
+									<div class="am-action-sheet-share-list-item-title">发短信</div>
+								</div>
+								<div class="am-action-sheet-share-list-item">
+									<div class="am-action-sheet-share-list-item-icon">
+										<i type="team" class="anticon anticon-team"></i>
+									</div>
+									<div class="am-action-sheet-share-list-item-title">发送到群</div>
+								</div>
+								<div class="am-action-sheet-share-list-item">
+									<div class="am-action-sheet-share-list-item-icon">
+										<i type="download" class="anticon anticon-download"></i>
+									</div>
+									<div class="am-action-sheet-share-list-item-title">下载</div>
+								</div>
+								<div class="am-action-sheet-share-list-item">
+									<div class="am-action-sheet-share-list-item-icon">
+										<i type="delete" class="anticon anticon-delete"></i>
+									</div>
+									<div class="am-action-sheet-share-list-item-title">删除</div>
+								</div>
+								<div class="am-action-sheet-share-list-item">
+									<div class="am-action-sheet-share-list-item-icon">
+										<i type="ellipsis" class="anticon anticon-ellipsis"></i>
+									</div>
+									<div class="am-action-sheet-share-list-item-title">更多</div>
+								</div>
+							</div>
 							<div class="am-action-sheet-share-cancel-button">取消</div>
 						</div>
-						
+
 					</div>
 				</div>
 
@@ -45,6 +82,10 @@
 			socMask
 		},
 		props: {
+			type: {
+				type: String,
+				default: 'default'
+			},
 			title: {
 				type: String,
 				default: ''
